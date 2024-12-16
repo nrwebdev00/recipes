71be2318309cecf8c2542aac350d5121c53ce5be
Nathon Reed <nrwebdev00@gmail.com>
2024-12-16 15:56:03 -0800

    able to change password and redirect to confromation page

--- account/urls.py
+++ account/urls.py
@@ -4,9 +4,23 @@ from . import views
 
 urlpatterns = [
     # path('login/', views.user_login, name='account_login'),
+
     # Login / Logout Views
     path('login/', auth_views.LoginView.as_view(), name='account_login'),
     path('logout/', auth_views.LogoutView.as_view(), name='account_logout'),
 
+    # Change Password Urls
+    path(
+         'password-change/',
+         auth_views.PasswordChangeView.as_view(),
+         name='password_change'
+    ),
+    path(
+        'password-change/done/',
+        auth_views.PasswordChangeDoneView.as_view(),
+        name='password_change_done'
+    ),
+
+    # Dashboard and Index view for Accounts
     path('', views.dashboard, name='account_dashboard'),
 ]
